078d1c99b50ac931ab57c22080a7e0c163e6eea3
brmilee <135774657+Brmilee@users.noreply.github.com>
2025-09-10 15:24:40 -0700

    feat: create Product API in backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js"
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {
-    res.send("Server is ready");
-});
+app.use(express.json()); // accepts JSON data in req.body
+
+app.use("/api/products", productRoutes);
 
 console.log(process.env.MONGO_URI);
 
