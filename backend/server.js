2aff4d25ddb0bb3cf3ba70ae6243d1ba7ea72b4c
brmilee <135774657+Brmilee@users.noreply.github.com>
2025-09-09 17:14:42 -0700

    feat: connect db to project

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log("Server starting at http://localhost:5000 Welcome" );
+app.get("/products", (req, res) => {
+    res.send("Server is ready");
 });
+
+console.log(process.env.MONGO_URI);
+
+app.listen(8080, () => {
+    connectDB();
+    console.log("Server starting at http://localhost:8080 Welcome");
+});
+
