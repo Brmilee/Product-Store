68ae4fbe0035eb0035a2eeaa1c088a7cea134b03
brmilee <135774657+Brmilee@users.noreply.github.com>
2025-09-19 20:28:10 -0700

    feat: add deployment logic

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js"
 
@@ -8,13 +9,20 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // accepts JSON data in req.body
+console.log("NODE_ENV:", process.env.NODE_ENV);
 
 app.use("/api/products", productRoutes);
 
-console.log(process.env.MONGO_URI);
-
-app.listen(8080, () => {
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend","dist")));
+    app.use((req, res) =>{
+        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    });
+}
+app.listen(PORT, () => {
     connectDB();
     console.log("Server starting at http://localhost:8080 Welcome");
 });
